45e3f193e0fe7b10563be464951d154917949ab2
sydneyreyn <sydney0709@gmail.com>
2018-08-17 14:01:59 +0100

    feed implementation onto profile page

--- client/components/feed.js
+++ client/components/feed.js
@@ -1,22 +1,74 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import gql from "graphql-tag";
+import { Avatar, icon } from "react-native-elements";
+import { Query } from "react-apollo";
+
+const GET_MY_PROFILE = gql`
+  query getMyProfile {
+    collections {
+      name
+      description
+      wines {
+        name
+        Description
+        price
+        type
+      }
+      settings {
+        name
+        Description
+      }
+      foods {
+        name
+        Description
+      }
+      others {
+        name
+        Description
+      }
+    }
+  }
+`;
+
 class Feed extends React.Component {
- render() {
-   return (
-     <View>
-       {this.props.posts.map(collection => {
-         return <Text style={styles.feedText}> {collection.text}</Text>;
-       })}
-     </View>
-   );
- }
+  render() {
+    return (
+      <View>
+        <Query query={GET_MY_PROFILE} fetchPolicy="network-only">
+          {({ loading, error, data }) => {
+            if (loading) {
+              return (
+                <View style={styles.loader}>
+                  <Image source={require("../assets/images/loader.gif")} />
+                </View>
+              );
+            }
+            if (error) {
+              if (error.message === "GraphQL error: Not authorized") {
+                this.props.navigation.navigate("Login");
+              }
+            }
+            console.log("here", error);
+            if (!data.collections) {
+              return <Text>"no collections found!"</Text>;
+            }
+            return (
+              data.collections.map(collection => {
+                return <Text style={styles.feedText} onPress={() => console.log("Clicked!")} > {collection.name} </Text>;
+              })
+            )
+          }}
+        </Query>
+      </View>
+    );
+  }
 }
 export default Feed;
 const styles = StyleSheet.create({
- feedText: {
-   backgroundColor: "#e0e0e0",
-   marginTop: 20,
-   padding: 50
- }
+  feedText: {
+    backgroundColor: "#e0e0e0",
+    marginTop: 20,
+    padding: 50
+  }
 });
-
--- client/screens/ProfileScreen.js
+++ client/screens/ProfileScreen.js
@@ -1,137 +1,112 @@
 import React from "react";
 import {
- View,
- Text,
- StyleSheet,
- Image,
- Button,
- AsyncStorage,
- StatusBar,
- data
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  AsyncStorage,
+  StatusBar,
+  data
 } from "react-native";
 import gql from "graphql-tag";
+import { Avatar, icon } from "react-native-elements";
 import { Query } from "react-apollo";
+import Feed from "../components/feed";
 
-const GET_MY_PROFILE = gql`
- query getMyProfile {
-   collection {
-       name
-       description
-       wines {
-           name
-           Description
-           price
-           type
-       }
-       settings {
-           name
-           Description
-       }
-       foods {
-           name
-           Description
-       }
-       others {
-           name
-           Description
-       }
-     }
- }
-`;
 export default class ProfileScreen extends React.Component {
- static navigationOptions = ({ navigation }) => {
-   const { state, navigate } = navigation;
-   return {
-     title: "Profile",
-     headerMode: "screen",
-     mode: "card"
-   };
- };
- render() {
-   return (
-     <View style={styles.container}>
-       <StatusBar barStyle="light-content" />
-       <Query query={GET_MY_PROFILE} fetchPolicy="network-only">
-         {({ loading, error, data }) => {
-           if (loading) {
-             return (
-               <View style={styles.loader}>
-                 <Image source={require("../assets/images/loader.gif")} />
-               </View>
-             );
-           }
-           if (error) {
-             if (error.message === "GraphQL error: Not authorized") {
-               this.props.navigation.navigate("Login");
-             }
-           }
-        //    if (!data.me) {
-        //      return <Text>"no data"</Text>;
-        //    }
-           return (
-             <View style={styles.profileHolder}>
-               <View style={styles.welcomeText}>
-               <Text> Welcome to your secure profile page!!!!</Text>
-               <Image source={require('../assets/images/icon.png')} />
-                 {/* <Text style={styles.profileName}>{data.collection.name}</Text>
+  static navigationOptions = ({ navigation }) => {
+    const { state, navigate } = navigation;
+    return {
+      title: "Profile",
+      headerMode: "screen",
+      mode: "card"
+    };
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+
+        <View style={styles.profileHolder}>
+          <View style={styles.welcomeText}>
+            <Text style={styles.welcomeText}> profile </Text>
+            <Avatar
+              style={styles.welcomeText}
+              medium
+              rounded
+              icon={{ name: "user" }}
+              onPress={() => console.log("Works!")}
+              activeOpacity={0.7}
+              // containerStyle={{flex: 2, marginLeft: 20, marginTop: 115}}
+            />
+            {/* <Text style={styles.profileName}>{data.collection.name}</Text>
                  <Text style={styles.profileDescription}>{data.collection.description}</Text> */}
-               </View>
-             </View>
-           );
-         }}
-       </Query>
-       <View style={styles.logoutHolder}>
-         <Button
-           title="Log out"
-           color="#8B008B"
-           onPress={async () => {
-             await AsyncStorage.removeItem("token");
-             this.props.navigation.navigate("Login");
-           }}
-         />
-       </View>
-     </View>
-   );
- }
+            <Text style={styles.subtitleText}>My collections: </Text>
+            <Feed />
+          </View>
+        </View>
+
+        <View style={styles.logoutHolder}>
+          <Button
+            title="Log out"
+            color="#8B008B"
+            onPress={async () => {
+              await AsyncStorage.removeItem("token");
+              this.props.navigation.navigate("Login");
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#fff",
-   justifyContent: "center"
- },
- welcomeText: {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  },
+  welcomeText: {
     fontSize: 40,
+    marginbottom: 30,
+    marginTop: 50,
     fontFamily: "Futura",
-    justifyContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
     fontWeight: "bold",
     color: "#8B008B"
   },
- profileHolder: {
-   flex: 1,
-   marginTop: 100,
-   flexWrap: "wrap",
-   flexDirection: "row",
-   justifyContent: "center",
-   justifyContent: "space-evenly"
- },
- profileName: {
-   fontSize: 30,
-   fontWeight: "bold",
-   color: "#911826"
- },
- profileDescription: {
-   fontSize: 20,
-   color: "#676767"
- },
- logoutHolder: {
-   marginBottom: 100
- },
- loader: {
-   flex: 1,
-   justifyContent: "center",
-   alignItems: "center"
- }
+  profileHolder: {
+    flex: 1,
+    marginTop: 10,
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "center",
+    justifyContent: "space-evenly"
+  },
+  subtitleText: {
+    fontSize: 15,
+    marginTop: 10,
+    fontFamily: "Futura",
+    justifyContent: "center",
+    color: "black",
+  },
+  profileName: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#8B008B"
+  },
+  profileDescription: {
+    fontSize: 20,
+    color: "#676767"
+  },
+  logoutHolder: {
+    marginBottom: 100
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
-  
